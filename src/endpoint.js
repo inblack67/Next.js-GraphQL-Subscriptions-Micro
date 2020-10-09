9914d2ee3508bc9f2f3033b75f2186c48aa0cfcc
Aman Bhardwaj <devproddeploy@gmail.com>
2020-10-09 21:12:08 +0530

    vercel is not happy

--- src/endpoint.js
+++ src/endpoint.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
-export const server = dev ? 'http://localhost:3000' : 'https://micro-subcriptions.vercel.app';
+export const server = dev ? 'http://localhost:3000' : 'https://micro-subcriptions.vercel.app:443';
 export const wsEndpoint = server.split('://')[1];
