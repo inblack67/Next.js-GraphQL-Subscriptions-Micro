131553e816809d4c4bd470f613e72d9a9fea8dd0
Aman Bhardwaj <devproddeploy@gmail.com>
2020-10-12 11:22:41 +0530

    some change

--- src/endpoint.js
+++ src/endpoint.js
@@ -1,2 +1,2 @@
 const dev = process.env.NODE_ENV !== 'production';
-export const server = dev ? 'localhost:3000' : process.env.SERVER_URL;
+export const server = dev ? 'localhost:3000' : process.env.NEXT_APP_SERVER_URL;
